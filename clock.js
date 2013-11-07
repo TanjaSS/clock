ecca0e2c6032d9544d6515177b854dde999a6c5f
Tanja <stricevictanja@gmail.com>
2013-11-07 19:44:07 +0100

    Show time

--- clock.js
+++ clock.js
@@ -1,3 +1,9 @@
 $(function() {
-    $("body").html("Hello world");
+    var time = new Date();
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+    $("#hours").html(hours);
+    $("#minutes").html(minutes);
+    $("#seconds").html(seconds);
 });
